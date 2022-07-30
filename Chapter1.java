121d0d7dfea791262113a2935f5b9bed5a5f5307
programmersyed <abrahamsyed10@gmail.com>
2022-07-30 07:32:11 -0400

    Whrw

--- Chapter1.java
+++ Chapter1.java
@@ -7,10 +7,17 @@ public class Chapter1 {
         String name = nam.nextLine();
         System.out.println("Welcome Detective "+name);
         System.out.println("It appears that the city needs you. Crime is rampant here in New Domino City and we are in need of a detective of your calibre");
-        System.out.println("Pick a number between one and 3");
-        int chap = nam.nextInt();
-        
+        System.out.println("Your first task will be against dirty dan");
+        System.out.println("Guess what mumber dan is thinking in order to catch him");
+        int chap= nam.nextInt();
+        while (chap != 3){
+            System.out.println("Guess again");
+            chap=nam.nextInt();
         }
+        System.out.println("Good guess detective");
+        System.out.println("Thanks to your help we have located Dirty Dan");
+        System.out.println("Its time for us to get him");
+        int[] dan = {12,14,15};
 
     }
 }
